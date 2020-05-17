49e08211cda730ba06ce53602c1645d6431974e4
Burns, Patrick <pburns@allegion.com>
2020-05-17 18:33:14 -0400

    header and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { render } from '@testing-library/react';
+import Header from './Header';
 
 class App extends Component {
     render() {
-      return (
-        <div>
-          HELLO WORLD
-        </div>
-    );
+      return <Header />;
   }
 }
 
